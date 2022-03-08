05716d3ba9f285319753c60a87fa60833d0349c1
Silvia Zeni <silvia.szn@gmail.com>
2022-03-08 14:30:28 +0000

    feat: Add setup files

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+const path = require("path");
+import { ViteAliases } from "vite-aliases";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    minify: false, // TODO maybe?
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "@gliff-ai/gliff-ts",
+      formats: ["es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [
+      ],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {},
+      },
+    },
+  },
+//   esbuild: {
+//     jsxInject: `import React from 'react'`,
+//   },
+  plugins: [ViteAliases()],
+});
